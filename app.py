88f89907044397f859fd76f385f625059c13b0d8
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-12-17 22:13:15 -0400

    add routes for separate pages

--- app.py
+++ app.py
@@ -5,5 +5,20 @@ app.config.from_object('config')
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/voice')
+def voice():
+    return render_template('voice.html')
+
+
+@app.route('/theatre')
+def theatre():
+    return render_template('theatre.html')
